43ba43aedb8800f913b0e6ac6a8b3ef3531b1c8e
Joe Haines <hello@joehaines.co.uk>
2017-01-22 22:09:44 +0000

    Start implementing run method

--- src/Console/RunCommand.php
+++ src/Console/RunCommand.php
@@ -2,10 +2,17 @@
 
 namespace Inphest\Framework\Console;
 
+use InvalidArgumentException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+
+use Inphest\Framework\AfterTestInterface;
+use Inphest\Framework\AfterSuiteInterface;
+use Inphest\Framework\BeforeTestInterface;
+use Inphest\Framework\BeforeSuiteInterface;
+use Inphest\Framework\TestSuiteConfigInterface;
 
 final class RunCommand extends Command
 {
@@ -21,22 +28,79 @@ final class RunCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output) : void
     {
-        $suiteConfig = $input->getArgument('suite_config');
+        $suiteConfigPath = $input->getArgument('suite_config');
 
         // 1. check file exists
+        if (!file_exists($suiteConfigPath)) {
+            throw new InvalidArgumentException(sprintf(
+                'Unable to find a config file at "%s"',
+                $suiteConfigPath
+            ));
+        }
+
         // 2. include file
         // 3. ensure implements TestSuiteConfigInterface
+        $suiteConfig = $this->getConfig($suiteConfigPath);
+
+        // TODO: move this logic to a TestRunner
+
+        // TODO: implement this as a decorator
         // 4. call beforeSuite if suite implements BeforeSuiteInterface
+        if ($suiteConfig instanceof BeforeSuiteInterface) {
+            $suiteConfig->beforeSuite();
+        }
+
         // 5. getTests()
         // 6. iterate over result and:
-        //    - call `suite::beforeTest` if suite implements BeforeTestInterface
-        //    - call `test::beforeTest` if test implements BeforeTestInterface
-        //    - call test method
-        //    - print test results
-        //    - call `test::afterTest` if test implements AfterTestInterface
-        //    - call `suite::afterTest` if suite implements AfterTestInterface
-        // 7. print suite results
-        // 8. call afterSuite if suite implements AfterSuiteInterface
+        foreach ($suiteConfig->getTests() as $test) {
+            // TODO: implement this as a decorator
+            //    - call `suite::beforeTest` if suite implements BeforeTestInterface
+            if ($suiteConfig instanceof BeforeTestInterface) {
+                $suiteConfig->beforeTest($test);
+            }
+
+            // TODO: implement this as a decorator
+            //    - call `test::beforeTest` if test implements BeforeTestInterface
+            if ($test instanceof BeforeTestInterface) {
+                $test->beforeTest($test);
+            }
+
+            // TODO
+            //    - get test methods (public methods starting with "test")
+            //    foreach test method:
+            //        - call test method
+            //        - print test results
+            //        - track if failure
+
+            // TODO: implement this as a decorator
+            //    - call `test::afterTest` if test implements AfterTestInterface
+            if ($test instanceof AfterTestInterface) {
+                $test->afterTest($test);
+            }
+
+            // TODO: implement this as a decorator
+            //    - call `suite::afterTest` if suite implements AfterTestInterface
+            if ($suiteConfig instanceof AfterTestInterface) {
+                $suiteConfig->afterTest($test);
+            }
+        }
+
+        // 7. call afterSuite if suite implements AfterSuiteInterface
+        if ($suiteConfig instanceof AfterSuiteInterface) {
+            $suiteConfig->afterSuite();
+        }
+
+        // end TestRunner code
+
+        // TODO
+        // 8. print suite results
+
+        // TODO
         // 9. set exit code
+    }
+
+    private function getConfig(string $path) : TestSuiteConfigInterface
+    {
+        return require $path;
     }
 }
