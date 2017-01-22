87a3f0074d6b12c5e38d0f95c5629bfc02dd62f9
Joe Haines <hello@joehaines.co.uk>
2017-01-22 20:12:57 +0000

    Start switching to simpler design

--- src/Console/RunCommand.php
+++ src/Console/RunCommand.php
@@ -2,63 +2,26 @@
 
 namespace Inphest\Framework\Console;
 
-use Iterator;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use Inphest\Framework\Factory\TestFileIteratorFactory;
-use Inphest\Framework\Factory\TestCaseFactory;
 
 final class RunCommand extends Command
 {
-    private const CONSOLE_WIDTH = 72;
-
-    public function __construct(
-        TestFileIteratorFactory $iteratorFactory,
-        TestCaseFactory $testCaseFactory
-    ) {
-        parent::__construct();
-
-        $this->iteratorFactory = $iteratorFactory;
-        $this->testCaseFactory = $testCaseFactory;
-    }
-
     protected function configure() : void
     {
         $this->setName('run')
             ->addArgument(
-                'directory',
+                'suite_config',
                 InputArgument::REQUIRED,
-                'Directory containing test files'
+                'Instance of TestSuiteConfigInterface containing configuration for the suite to run'
             );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) : void
     {
-        $before = time();
-
-        $directory = $input->getArgument('directory');
-        $iterator = $this->iteratorFactory->create($directory);
-
-        $tests = 0;
-
-        foreach ($iterator as $path => $file) {
-            $test = $this->testCaseFactory->create($file);
-            $numberOfTests = $test->count();
-
-            $test->runTests();
-
-            $output->writeln($test->getName() . ' (' . $numberOfTests . ')');
-
-            $tests += $numberOfTests;
-        }
-
-        $after = time();
-
-        $output->writeln(PHP_EOL);
-        $output->writeln('Ran ' . $tests . ' tests in ' . ($after - $before) . 's');
     }
 }
