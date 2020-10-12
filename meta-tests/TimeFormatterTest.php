48fcab1a75fdcb85efd88bc7a9f654a60e588e5d
Joe Haines <hello@joehaines.co.uk>
2020-10-12 16:41:13 +0100

    Improve time formatter test labels

--- meta-tests/TimeFormatterTest.php
+++ meta-tests/TimeFormatterTest.php
@@ -8,50 +8,50 @@ use Inphest\Assert;
 use Inphest\Internal\TimeFormatter;
 use function Inphest\test;
 
-test('FormatZero', function (Assert $assert): void {
+test('0 is formatted as "0ms"', function (Assert $assert): void {
     $assert->same('0ms', TimeFormatter::format(0));
 });
 
-test('Format1Millisecond', function (Assert $assert): void {
+test('1 millisecond is formatted as "1ms"', function (Assert $assert): void {
     $assert->same('1ms', TimeFormatter::format(1_000_000));
 });
 
-test('Format99Millisecond', function (Assert $assert): void {
+test('99 milliseconds is formatted as "99ms"', function (Assert $assert): void {
     $assert->same('99ms', TimeFormatter::format(99_000_000));
 });
 
-test('Format999Millisecond', function (Assert $assert): void {
+test('999 milliseconds is formatted as "999ms"', function (Assert $assert): void {
     $assert->same('999ms', TimeFormatter::format(999_000_000));
 });
 
-test('Format1Second', function (Assert $assert): void {
+test('1 second is formatted as "1s"', function (Assert $assert): void {
     $assert->same('1s', TimeFormatter::format(1_000_000_000));
 });
 
-test('Format59Seconds', function (Assert $assert): void {
+test('59 seconds is formatted as "59s"', function (Assert $assert): void {
     $assert->same('59s', TimeFormatter::format(59_000_000_000));
 });
 
-test('Format59Seconds99Milliseconds', function (Assert $assert): void {
+test('59 seconds & 99 milliseconds is formatted as "59s 99ms"', function (Assert $assert): void {
     $assert->same('59s 99ms', TimeFormatter::format(59_099_000_000));
 });
 
-test('Format59Seconds999Milliseconds', function (Assert $assert): void {
+test('59 seconds & 999 milliseconds is formatted as "59s 999ms', function (Assert $assert): void {
     $assert->same('59s 999ms', TimeFormatter::format(59_999_000_000));
 });
 
-test('Format1Minute', function (Assert $assert): void {
+test('1 minute is formatted as "1m"', function (Assert $assert): void {
     $assert->same('1m', TimeFormatter::format(60_000_000_000));
 });
 
-test('Format10Minutes', function (Assert $assert): void {
+test('10 minutes is formatted as "10m"', function (Assert $assert): void {
     $assert->same('10m', TimeFormatter::format(600_000_000_000));
 });
 
-test('Format12Minutes34Seconds56Milliseconds', function (Assert $assert): void {
+test('12 minutes, 34 seconds & 56 milliseconds is formatted as "12m 34s 56ms"', function (Assert $assert): void {
     $assert->same('12m 34s 56ms', TimeFormatter::format(754_056_000_000));
 });
 
-test('Format11Minutes11Seconds999Milliseconds', function (Assert $assert): void {
+test('11 minutes, 11 seconds & 999 milliseconds is formatted as "11m 11s 999ms"', function (Assert $assert): void {
     $assert->same('11m 11s 999ms', TimeFormatter::format(671_999_000_000));
 });
